a09c5f375bc6a04485b75fc4ccc52dfd868556c9
lucas daniel <146143426+lucassdani@users.noreply.github.com>
2023-11-15 15:10:45 -0300

    n/a

---
+++ javascript-modulo-1/exercicio/ex01/index.js
@@ -0,0 +1,8 @@
+let primeiro_nome = 'lucas '
+let sobrenome = 'daniel'
+let campo_de_estudo = 'javascript'
+let ano_nascimento = 2007
+
+console.log(primeiro_nome + sobrenome)
+console.log(campo_de_estudo)
+console.log(ano_nascimento = 2023 - ano_nascimento)
