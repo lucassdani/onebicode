3095ba638056a8433da1a257587aa8f3e7ead6cb
lucas daniel <146143426+lucassdani@users.noreply.github.com>
2023-11-15 14:51:25 -0300

    n/a

---
+++ javascript-modulo-1/aula-10/index.js
@@ -0,0 +1,11 @@
+let nome = 'lucas'
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+// window.alert(nome)
+// nome = window.prompt('insira o seu nome: ')
+// let confirmacao = window.confirm('confirma essa ação?')
+
+alert('ola, mundo')
