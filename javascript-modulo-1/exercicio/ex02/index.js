813abad98e38d4c3c1e894c822e985c9cbdd592a
lucas daniel <146143426+lucassdani@users.noreply.github.com>
2023-12-06 18:17:42 -0300

    n/a

--- javascript-modulo-1/exercicio/ex02/index.js
+++ javascript-modulo-1/exercicio/ex02/index.js
@@ -1,12 +1,12 @@
-const carro1 = prompt('nome do carro 1:')
-const carro1_velocidade= parseFloat(prompt('qual a velocidade?:'))
-const carro2 = prompt('nome do carro 2:')
-const carro2_velocidade = parseFloat(prompt('qual a velocidade?:'))
+const carro = prompt('nome do carro 1:')
+let velocidade1 = parseFloat(prompt('qual a velocidade do carro?'))
+const carro2 = prompt('qual o nome do carro 2')
+let velocidade2 = parseFloat(prompt('qual a velocidade do carro?'))
 
-if (carro1_velocidade > carro2_velocidade) {
-    alert(carro1 + ' é mais rapido')
-} else if (carro1_velocidade > carro1_velocidade){
-    alert(carro2 + ' é mais rapido')
-} else {
-    alert(carro1 + ' ' + carro2 + ' tem a mesma velociade')
+if (velocidade1 > velocidade2) {
+    alert(carro + ' é mais rapido')
+} else if (velocidade1 < velocidade2) {
+    alert(carro2 + ' é mais rápido')
+}else if (velocidade1 === velocidade2){
+    alert('os dois carros tem a mesma velocidade')
 }
