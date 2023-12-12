bda1910fa412c572b8798ea839b4b726dda25da2
lucas daniel <146143426+lucassdani@users.noreply.github.com>
2023-12-12 10:33:35 -0300

    n/a

---
+++ javascript-modulo-1/exercicio/ex01/cadastro.js
@@ -0,0 +1,10 @@
+let nome = prompt('digite seu nome:')
+let sobrenome = prompt('digite seu sobrenome:')
+let campoDeEstudo = prompt('digite seu campo de estudo:')
+let AnoDeNascimento = parseFloat(prompt('digite o seu ano de nascimento:'))
+
+alert(
+    nome + ' ' + sobrenome +
+    '\n' + campoDeEstudo +
+    '\n' + AnoDeNascimento - 2023
+)
