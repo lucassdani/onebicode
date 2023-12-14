3609fdae2909483cc1b125401a6912827a44c2d7
lucas daniel <146143426+lucassdani@users.noreply.github.com>
2023-12-14 07:22:34 -0300

    n/a

---
+++ javascript-modulo-1/aula-16/index.js
@@ -0,0 +1,13 @@
+// const idade = prompt('informe sua idade:')
+
+// if (idade > 18) {
+//     alert('você é maior de idade')
+// }else if (idade > 12){
+//     alert('você é menor de idade')
+// }else if (idade > 4) {
+//     alert('você é criança')
+// }else {
+//     alert('...')
+// }
+
+const resultado = (6 === 6) ? 'verdadeiro' : 'falso'
