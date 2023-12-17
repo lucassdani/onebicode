439a3b5fffb63bb7b48d5c8f401bc7291b025ab3
lucas daniel <146143426+lucassdani@users.noreply.github.com>
2023-12-17 14:48:54 -0300

    n/a

--- javascript-modulo-1/aula-19/switch.js
+++ javascript-modulo-1/aula-19/switch.js
@@ -1,17 +1,17 @@
-const resultado = prompt('escolha uma alternativa:\na)\nb),\nc)')
+const resultado = prompt('escolha uma alternativa: \na)\nb)\nc)')
 
 const resultadoNumerico = parseFloat(resultado)
 
 switch (resultadoNumerico) {
-    case 1:
-        alert('resultado é "a"')
+    case 1: 
+        alert('o resultado é "a"')
         break
-    case 2:
-        alert('resultado é "b"')
+    case 2: 
+        alert('o resultado é "b"')
         break
     case 3: 
-        alert('o resultado é "c')
+        alert('o resultado é "c"')
         break
-    default:
-        alert('finalizano..')
+    default: 
+        alert('escolha uma das opções')
 }
