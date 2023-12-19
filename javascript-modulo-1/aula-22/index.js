265e5fb6015efd243fc86054881a72612b844af1
lucas daniel <146143426+lucassdani@users.noreply.github.com>
2023-12-19 12:38:18 -0300

    n/a

--- javascript-modulo-1/aula-22/index.js
+++ javascript-modulo-1/aula-22/index.js
@@ -1,13 +1,9 @@
-let velocidade = 80
+let velocidade = 80 
 
 while (velocidade > 0) {
-    alert('o carro esta a ' + velocidade + 'km/h' )
+    alert('o carro esta a ' + velocidade + ' km/h')
     velocidade -= 20
     alert('diminuindo 20 km/h')
-
-    if (velocidade === 40) {
-        break
-    }
 }
 
 alert('o carro parou')
