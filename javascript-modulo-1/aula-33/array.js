268bad5af39de0aed541276af3cee380b29fe2bc
lucas daniel <146143426+lucassdani@users.noreply.github.com>
2024-02-16 11:30:02 -0300

    n/a

--- javascript-modulo-1/aula-33/array.js
+++ javascript-modulo-1/aula-33/array.js
@@ -2,51 +2,53 @@ const arr = ["Frodo", "Sam", "Merry", "Pippin", "Gandalf", "Aragorn", "Legolas",
 console.log(arr)
 
 // adicionar elementos
-// push
+//push: Adiciona um elemento no final do array e devolve o novo tamanho do array
 let tamanho = arr.push('boromir')
 console.log(arr)
 console.log(tamanho)
 
-//unshift
+
+// unshift: Adiciona um elemento no começo do array e devolve o novo tamanho do array
 tamanho = arr.unshift('teste')
 console.log(arr)
 console.log(tamanho)
 
 
 // remover elementos
-// pop
+// pop: Remove um elemento no final do array e devolve o elemento removido
 let ultimoElemento = arr.pop()
 console.log(arr)
 console.log(ultimoElemento)
 
-// shift
+//shift: Remove um elemento no começo do array e devolve o elemento removido
 ultimoElemento = arr.shift()
 console.log(arr)
 console.log(ultimoElemento)
 
 //pesquisar por um elemento
-// includes
+// includes: Verifica se um certo elemento está presente no array 
 const inclui = arr.includes("Gandalf")
 console.log(inclui)
 
-// indexof
+// indexOf: Encontra o índice que possui o elemento indicado, ou -1 caso ele não exista no array
 const indice = arr.indexOf("Gandalf")
 console.log(indice)
 
 // cortar e concatenar
-// slice
+// slice: Copia uma parte do array e devolve a parte copiada sem alterar o array original
 const hobbit = arr.slice(0, 4)
+// Também pode ser usado com números negativos referenciar o final do array
 const outros = arr.slice(-4)
 console.log(arr)
 console.log(hobbit)
 console.log(outros)
 
-//concat
+// concat: Junta dois ou mais arrays e/ou itens e devolve o resultado (sem alterar nenhum dos arrays)
 const sociedade =hobbit.concat(outros, 'boromir')
 console.log(sociedade)
 
 // substituição dos elementos
-// splice
+// splice: Permite remover elementos em qualquer posição do array e substituir por novos
 const elementosRemovidos = sociedade.splice(indice, 1, 'gadalf, o cinzento')
 console.log(sociedade)
 console.log(elementosRemovidos)
---
+++ javascript-modulo-1/exercicio/ex07/fila-de-espera.js
@@ -0,0 +1,34 @@
+let fila = [];
+let opcao = "";
+
+do {
+  let pacientes = "";
+  for (let i = 0; i < fila.length; i++) {
+    pacientes += `${i + 1}° - ${fila[i]} \n`;
+  }
+
+  opcao = prompt(
+    `pacientes\n ${pacientes}
+        \nescolha uma ação:\n1. novo paciente\n2. consultar\n3. sair`
+  );
+
+  switch (opcao) {
+    case "1":
+        const novoPaciente = prompt("qual o nome do paciente?");
+        fila.push(novoPaciente);
+        break
+    case '2':
+        const pacienteConsultado = fila.shift()
+        if (pacienteConsultado) {
+            alert(`${pacienteConsultado} foi removido da fila.`)
+        }else {
+            alert('não há pacientes na fila!')
+        }
+        break
+    case '3':
+        alert('encerrando...')
+        break
+    default:
+        alert('opção inválida!')
+  }
+} while (opcao !== "3");
