668f9b2bc5b055e77df143ffbc23c352e66ea02f
lucas daniel <146143426+lucassdani@users.noreply.github.com>
2025-04-25 12:23:09 -0300

    n/a

--- javascript-modulo-1/aula-44/escopo.js
+++ javascript-modulo-1/aula-44/escopo.js
@@ -1,12 +1,19 @@
 let pokemon = 'charmander'
 
 function evoluir() {
-    pokemon = 'charmileon'
+    pokemon = 'charmeleon'
 }
 
-// console.log(pokemon)
-// evoluir()
-// console.log(pokemon)
+console.log(pokemon)
+evoluir()
+console.log(pokemon)
+
+function criarAnimal() {
+    let animal = 'gato'
+}
+
+criarAnimal()
+//console.log(animal())
 
 function avaliarNota(nota) {
     if (nota > 60) {
@@ -18,9 +25,24 @@ function avaliarNota(nota) {
     }
 
     console.log(nota)
-    consol.log(aprovado)
+    console.log(aprovado)
     console.log(situacao)
 }
 
-avaliarNota(83)
-avaliarNota(49)
+//avaliarNota(83)
+//avaliarNota(49)
+
+function ola() {
+    var texto = 'ola mundo!'
+}
+
+//console.log(texto)
+
+console.log(nome)
+console.log(sobrenome)
+
+var nome = 'lucas'
+let sobrenome = 'daniel'
+
+console.log(nome)
+console.log(sobrenome)
