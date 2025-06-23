fdbb6ab4b8e19e0173dbe8a5dd41bcc77757035e
lucas daniel <146143426+lucassdani@users.noreply.github.com>
2025-06-23 15:47:57 -0300

    n/a

--- javascript-modulo-1/aula-48/recursao.js
+++ javascript-modulo-1/aula-48/recursao.js
@@ -1,31 +1,27 @@
 function dividir(num) {
-  console.log(num);
-  if (num % 2 === 0) {
-    dividir(num / 2);
+  console.log(num)
+  if (num % 2 ===0) {
+    dividir(num / 2)
   } else {
-    return num;
+    return num
   }
 }
 
-// dividir(256)
+dividir(1000)
 
 function dobrar(num) {
-  console.log(num);
-  dobrar(num * 2);
+  console.log(num)
+  dobrar(num *2)
 }
 
-// dobrar(1)
+//dobras(1)
 
+//fatoria de 5 -> !5 === 5 * 4 * 3 * 2 * 1 === 5 * !4 
 function fatorial(num) {
   console.log(`Número: ${num}`)
   if (num === 0) { // caso base
     return 1
-  } else if (num === 1) {
-    return 1
   } else {
-    console.log(`${num} * !${(num-1)}`)
-    return num * fatorial(num - 1)
+    
   }
 }
-
-console.log(fatorial(5))
