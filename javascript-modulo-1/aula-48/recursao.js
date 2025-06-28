b71a73bbaa8447a11f320aeee72eedeb3a1dfa47
lucas daniel <146143426+lucassdani@users.noreply.github.com>
2025-06-28 15:25:32 -0300

    n/a

--- javascript-modulo-1/aula-48/recursao.js
+++ javascript-modulo-1/aula-48/recursao.js
@@ -21,7 +21,13 @@ function fatorial(num) {
   console.log(`Número: ${num}`)
   if (num === 0) { // caso base
     return 1
+  } else if (num === 1) {
+    return 1
   } else {
-    
+    console.log(`${num} * ! ${num - 1}`)
+    return num * fatorial(num - 1)
   }
+    
 }
+
+console.log(fatorial(5))
