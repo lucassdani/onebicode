2cbc52730b609d2db4c5573b77a6338f51ea3a19
lucas daniel <146143426+lucassdani@users.noreply.github.com>
2025-07-10 09:31:52 -0300

    n/a

---
+++ javascript-modulo-1/aula-49/funcoes-anonimas.js
@@ -0,0 +1,24 @@
+/*function somar(a, b) {
+    return a + b
+}
+
+const operacao = somar
+
+console.log(operacao(4, 5))
+
+operacao = function (a, b) {
+    return a - b
+}
+
+console.log(operacao(36, 13))*/
+
+olamundo()
+oimundo()
+
+function olamundo() {
+    console.log('ola mundo')
+}
+
+const oimundo = function () {
+    console.log('oi mundo')
+}
