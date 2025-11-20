fa02c1846541e770d09c3e99c6489233742d7295
lucas daniel <146143426+lucassdani@users.noreply.github.com>
2025-11-20 15:59:58 -0300

    n/a

--- javascript-modulo-2/exercicios/ex011/index.js
+++ javascript-modulo-2/exercicios/ex011/index.js
@@ -1,31 +1,39 @@
 function addPlayer() {
     const position = document.getElementById('position').value
     const name = document.getElementById('name').value
-    const number = document.getElementById('playerNumber').value
+    const number = document.getElementById('number').value
 
-    const confirmation = confirm(`Escalar ${name} como ${position}?`)
+    const confirmation = confirm('deseja confirmar jogador?')
     
-    if (confirmation) {
-        const teamList = document.getElementById('teamList')
-        const playerItem = document.createElement('li')
-        playerItem.id = `player-${number}`
-        playerItem.innerText = `${position}: ${name}(${number})` 
-        teamList.appendChild(playerItem)
-
-         document.getElementById('position').value = ''
-         document.getElementById('name').value = ''
-         document.getElementById('number').value = ''
-    }
-}
-
-function removerPlayer() {
-    const number = document.getElementById('numberToRemove').value
-    const playerRemove = document.getElementById(`player-${number}`)
-
-    const confirmation = confirm(`remover o número ${number}`)
-
-    if (confirmation) {
-        playerRemove.remove()
+    if (confirmation === true) {
+        const ul = document.getElementById('teamList')
+        const info = document.createElement('li')
+        info.id = `player - ${number}`
+        info.innerText = `${position}: ${name} (${number})`
+        ul.appendChild(info)
+        document.getElementById('position').value = ''
+        document.getElementById('name').value = ''
         document.getElementById('number').value = ''
     }
 }
+
+const button = document.getElementById('tofield')
+
+button.addEventListener("click", addPlayer);
+
+function removeplayer() {
+    const Number = document.getElementById('removeNumber')
+    const playeremove = document.getElementById(`player - ${number}`)
+
+    const confirmation = confirm('remover jogador?')
+
+    if (confirmation) {
+        document.getElementById('ul').removeChild(ul)
+        document.getElementById('removeNumber').value = ''
+    }
+    
+}
+
+const removeButton = document.getElementById('remove')
+
+removeButton.addEventListener('click', removeplayer)
